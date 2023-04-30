ecb8e10e4a885855e5e5f657b3328d1ca319a855
Jogesh Gupta <88982643+jogeshgupta963@users.noreply.github.com>
2023-05-01 00:23:34 +0530

    Create look.cpp

---
+++ look.cpp
@@ -0,0 +1,29 @@
+int look (vector<int> v,int head,int dir){
+    vector<int> left;
+    vector<int> right;
+    
+    for(int i:v){
+        if(i < head){
+            left.push_back(i);
+        }else{
+            right.push_back(i);
+        }
+    }
+    
+    sort(left.begin(),left.end());
+    sort(right.begin(),right.end());
+    
+    if(dir == 1){
+        int dis =  abs (head -  right[right.size()-1]) + abs(right[right.size()-1] - left[0] );
+        return dis;
+    }
+    else{
+        int dis =  abs (head - left[0] ) + right[right.size()-1] - left[0] ;
+        return dis;
+        
+    }
+}
+
+int main(){
+    cout<< look({82,170,43,140,24,16,190},50,-1);
+}
